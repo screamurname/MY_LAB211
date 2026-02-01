bebd818e1b4dc31f5460d9a78c34cc11b68f391b
screamurname <nguyendangvu444@gmail.com>
2026-02-01 22:24:01 +0700

    Update Main.java

--- Fibonacci/Main.java
+++ Fibonacci/Main.java
@@ -1,25 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Fibonacci4;
 
-/**
- *
- * @author admin
- */
 public class Main {
     public static void main(String[] args) {
-    FibonacciModel model = new FibonacciModel();
-    FibonacciView view = new FibonacciView();
-    
-    FibonacciController controller = new FibonacciController(model, view);
-    
-    
-    controller.run();
-
+        // Khởi tạo các thành phần
+        FibonacciModel model = new FibonacciModel();
+        FibonacciView view = new FibonacciView();
+        
+        // Kết nối chúng qua Controller
+        FibonacciController controller = new FibonacciController(model, view);
+        
+        // Chạy chương trình
+        controller.run();
     }
-
-
-
 }
